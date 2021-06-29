689d0b87998eb4d5fc69f32fcbe53605bd235eb3
Desiree_McElroy <desireeashley90@gmail.com>
2021-06-29 15:33:30 -0500

    adding acquire functions

---
+++ acquire.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+
+import os
+import requests
+
+
+def get_df(name):
+    """
+    This function takes in the string 'items', 'stores', or 'sales' and
+    returns a df containing all pages and creates a .csv file for future use.
+    """
+    base_url = 'https://python.zach.lol'
+    api_url = base_url + '/api/v1/'
+    response = requests.get(api_url + name)
+    data = response.json()
+    
+    # create list from 1st page
+    my_list = data['payload'][name]
+    
+    # loop through the pages and add to list
+    while data['payload']['next_page'] != None:
+        response = requests.get(base_url + data['payload']['next_page'])
+        data = response.json()
+        my_list.extend(data['payload'][name])
+    
+    # Create DataFrame from list
+    df = pd.DataFrame(my_list)
+    
+    # Write DataFrame to csv file for future use
+    df.to_csv(name + '.csv')
+    return df
+
+
+
+def get_germany():
+    """
+    This function uses or creates the opsd_germany_daily csv and returns a df.
+    """
+    if os.path.isfile('opsd_germany_daily.csv'):
+        df = pd.read_csv('opsd_germany_daily.csv', index_col=0)
+    else:
+        url = 'https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv'
+        df = pd.read_csv(url)
+        df.to_csv('opsd_germany_daily.csv')
+    return df
