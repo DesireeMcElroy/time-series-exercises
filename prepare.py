ab51f2c899c3be536fbb77477ea97b4ca2fa7d63
Desiree_McElroy <desireeashley90@gmail.com>
2021-07-01 16:55:03 -0500

    adding prepare

---
+++ prepare.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import acquire
+import requests
+import os
+
+from datetime import timedelta, datetime
+
+def prep_items(df):
+    '''
+    This function takes in dataframe and drops unnecessary columns, adds a month, weekday and sales_total column
+    '''
+    # drop extra columns
+    df.drop(columns=['Unnamed: 0_x', 'Unnamed: 0_y', 'Unnamed: 0'], inplace=True)
+
+    # change date column to datetime
+    df.sale_date = pd.to_datetime(df.sale_date)
+
+    # change date to index
+    df = df.set_index('sale_date').sort_index()
+
+    # create month and column year
+    df['month'] = df.index.month
+    df['weekday'] = df.index.day_name()
+
+    # create sales total column
+    df['sales_total'] = df.sale_amount*df.item_price
+
+    return df
+
+
+def prep_germany(df):
+    '''
+    This function takes in a dataframe and sets the date as index, creates and month and year column,
+    and fills all the nulls with the mean average of that column
+    '''
+    
+    # convert Date column to datetime
+    df.Date = pd.to_datetime(df.Date)
+    
+    # set date column as index
+    df = df.set_index('Date').sort_index()
+    
+    # make a month and year column
+    df['month'] = df.index.month
+    df['year'] = df.index.year
+    
+    # fill nulls with mean average
+    values = {'Consumption': df.Consumption.mean(),
+          'Wind': df.Wind.mean(),
+          'Solar': df.Solar.mean(),
+          'Wind+Solar': df['Wind+Solar'].mean()}
+
+    df.fillna(value=values, inplace=True)
+    
+    return df
